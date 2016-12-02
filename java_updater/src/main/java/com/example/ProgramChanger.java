35e940f42e1190ca36ee8526ad96196641e90a4e
Anton <a.belichenko@tecsynt.com>
2016-12-02 18:18:57 +0200

    add thread pool

--- java_updater/src/main/java/com/example/ProgramChanger.java
+++ java_updater/src/main/java/com/example/ProgramChanger.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 /**
  * Created by Belichenko Anton on 02.12.16.
@@ -14,13 +17,13 @@ import java.util.logging.Logger;
 
 public class ProgramChanger {
 
-    Logger log = Logger.getLogger(ProgramChanger.class.getName());
-    private static final int NUM_THREADS = 1;
-    private static final int SLEEP_MINUTES = 1;
+    private static final long SLEEP = 10;
     private boolean isInAndroidStudio;
     private int currentProgramIndex;
-    private String[] programList = {"/Applications/Android Studio.app",
-            "/Applications/Google Chrome.app"};
+
+    private String[] programList = {
+            "/Applications/Google Chrome.app",
+            "/Applications/Android Studio.app"};
 
     public static final String ANDROID_STUDIO = "Android Studio";
 
@@ -32,9 +35,9 @@ public class ProgramChanger {
         if (currentProgramIndex + 1 > programList.length) {
             currentProgramIndex = 0;
         } else {
-            currentProgramIndex += 1;
+            currentProgramIndex = currentProgramIndex + 1;
         }
-        System.out.print("In to change method - "+programList[currentProgramIndex]);
+        System.out.println("In to change method - " + programList[currentProgramIndex]);
         try {
             Desktop.getDesktop().open(new File(programList[currentProgramIndex]));
             if (programList[currentProgramIndex].contains(ANDROID_STUDIO)) {
@@ -42,22 +45,29 @@ public class ProgramChanger {
             } else {
                 isInAndroidStudio = false;
             }
-            System.out.print("Change program successfully");
+            System.out.println("Change program successfully");
         } catch (IOException e) {
-            System.out.print("in exception "+ e.toString());
+            System.out.println("in exception " + e.toString());
             currentProgramIndex = 0;
             e.printStackTrace();
         }
     }
 
     public void start() {
-        final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(NUM_THREADS);
-        executor.scheduleAtFixedRate(new Runnable() {
+
+        final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("programChange")
+                .setDaemon(true)
+                .build();
+
+        final ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor(threadFactory);
+        service.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
                 changeProgram();
             }
-        }, SLEEP_MINUTES,  SLEEP_MINUTES, TimeUnit.MINUTES);
+
+        }, 10, SLEEP, TimeUnit.SECONDS);
     }
 
 }
